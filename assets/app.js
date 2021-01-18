0d9e502fb3a5780cc1fe4d362d5ab7df4d34f315
Ernest <ernestwisniewski2@gmail.com>
2021-01-18 09:55:19 +0100

    Webcore recipe update

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,22 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import Vue from 'vue';
+import VueTest from './components/VueTest'
+import './styles/app.scss';
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+require('bootstrap');
 
 // start the Stimulus application
 import './bootstrap';
+
+// Vuejs
+Vue.options.delimiters = ['${', '}$']
+
+let vue = new Vue({
+    el: '#kbin',
+    data: {
+        message: 'test'
+    },
+    components: {
+        VueTest
+    },
+});
+console.log(vue);
