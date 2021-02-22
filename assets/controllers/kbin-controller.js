bb701a9423c187c3e764577e4311bb4cc72e5a66
Ernest <ernestwisniewski2@gmail.com>
2021-02-22 22:46:58 +0100

    Some darkstyles improvements

--- assets/controllers/kbin-controller.js
+++ assets/controllers/kbin-controller.js
@@ -1,10 +1,21 @@
 import {Controller} from 'stimulus';
 import {fetch, ok} from "./utils/http";
+import Cookies from 'js-cookie';
 
 export default class extends Controller {
+    connect() {
+        if (Cookies.get('dark')) {
+            this.element.classList.add('kbin-dark');
+        }
+    }
 
     dark() {
-        this.element.classList.toggle('kbin-dark')
+        this.element.classList.toggle('kbin-dark');
+        if (Cookies.get('dark')) {
+            Cookies.remove('dark');
+        } else {
+            Cookies.set('dark', true);
+        }
     }
 
 }
