86d3fc56b70bd041859ab661704d1d50f46a2389
Ernest <ernestwisniewski2@gmail.com>
2021-03-11 22:29:01 +0100

    Scroll progress

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -6,12 +6,15 @@ import KChoices from './controllers/utils/choices';
 import KDatepicker from './controllers/utils/datepicker';
 import KEditor from './controllers/utils/editor';
 import KLoginAlert from './controllers/utils/login-alert';
+import ScrollProgress from "stimulus-scroll-progress"
+
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context('./controllers', true, /\.(j|t)sx?$/));
 
 app.register('reveal', Reveal);
 app.register('clipboard', Clipboard);
+app.register("scroll-progress", ScrollProgress)
 
 window.addEventListener('load', function(event) {
     const choices = new KChoices();
