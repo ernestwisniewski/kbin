02293a792f26a563cd6b941d7e013374fc8e392f
Ernest <ernestwisniewski2@gmail.com>
2021-03-14 16:04:17 +0100

    Image loader cursor

--- assets/controllers/image-controller.js
+++ assets/controllers/image-controller.js
@@ -1,7 +1,10 @@
-import { ApplicationController } from 'stimulus-use'
-import { useDispatch } from 'stimulus-use'
+import {ApplicationController} from 'stimulus-use'
+import {useDispatch} from 'stimulus-use'
+import {fetch, ok} from "./utils/http";
+import KEditor from "./utils/editor";
 
 export default class extends ApplicationController {
+    static classes = ['loading']
     static values = {
         loading: Boolean,
         isFull: Boolean,
@@ -9,19 +12,26 @@ export default class extends ApplicationController {
         thumb: String
     };
 
-    connect() {
-        useDispatch(this);
-    }
-
-    toggle() {
+    async toggle() {
         if (this.isFullValue) {
             this.element.src = this.thumbValue;
             this.isFullValue = false;
         } else {
             this.dispatch('expand', 'true')
 
-            this.element.src = this.fullValue;
-            this.isFullValue = true;
+            this.loadingValue = true;
+            this.element.classList.add(this.loadingClass)
+            try {
+                let response = await fetch(this.fullValue, {method: 'GET'});
+                response = await ok(response);
+
+                this.element.src = this.fullValue;
+                this.isFullValue = true;
+            } catch (e) {
+            } finally {
+                this.loadingValue = false;
+                this.element.classList.remove(this.loadingClass)
+            }
         }
     }
 }
