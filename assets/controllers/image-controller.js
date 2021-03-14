afa0bb7b0bc804bb0da3bb29b03385c77c57dfe1
Ernest <ernestwisniewski2@gmail.com>
2021-03-14 16:30:09 +0100

    Image front controller

--- assets/controllers/image-controller.js
+++ assets/controllers/image-controller.js
@@ -1,7 +1,4 @@
 import {ApplicationController} from 'stimulus-use'
-import {useDispatch} from 'stimulus-use'
-import {fetch, ok} from "./utils/http";
-import KEditor from "./utils/editor";
 
 export default class extends ApplicationController {
     static classes = ['loading']
@@ -20,18 +17,22 @@ export default class extends ApplicationController {
             this.dispatch('expand', 'true')
 
             this.loadingValue = true;
-            this.element.classList.add(this.loadingClass)
-            try {
-                let response = await fetch(this.fullValue, {method: 'GET'});
-                response = await ok(response);
 
-                this.element.src = this.fullValue;
-                this.isFullValue = true;
-            } catch (e) {
-            } finally {
-                this.loadingValue = false;
-                this.element.classList.remove(this.loadingClass)
-            }
+            this.element.src = this.fullValue;
+            this.isFullValue = true;
+
+            let self = this;
+            this.element.onload = (() => {
+                self.loadingValue = false;
+            })
+        }
+    }
+
+    loadingValueChanged() {
+        if (this.loadingValue) {
+            this.element.classList.add(this.loadingClass)
+        } else {
+            this.element.classList.remove(this.loadingClass)
         }
     }
 }
