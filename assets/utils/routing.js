f573f5b9ffa39ada600f848ba17a7d6988081683
Ernest <ernestwisniewski2@gmail.com>
2021-03-18 01:39:50 +0100

    Fix routing paths

--- assets/utils/routing.js
+++ assets/utils/routing.js
@@ -1,5 +1,5 @@
-const routes = require('../../../public/js/fos_js_routes.json');
-import Routing from '../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
+const routes = require('../../public/js/fos_js_routes.json');
+import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
 
 export default function router() {
     Routing.setRoutingData(routes);
