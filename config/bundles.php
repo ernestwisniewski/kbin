e2bdaadf514c6a537c94433f59cb17f715e0267a
Ernest <ernestwisniewski2@gmail.com>
2021-03-22 15:21:09 +0100

    Update dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -27,4 +27,5 @@ return [
     Tchoulom\ViewCounterBundle\TchoulomViewCounterBundle::class => ['all' => true],
     Karser\Recaptcha3Bundle\KarserRecaptcha3Bundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
