2b79ec87882e50d42a57c5d1ecaafc1ead901d3e
Ernest <ernestwisniewski2@gmail.com>
2021-03-25 15:00:42 +0100

    Message test init

--- tests/Controller/MessageControllerTest.php
+++ tests/Controller/MessageControllerTest.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace App\Tests\Controller;
 
@@ -7,5 +7,77 @@ use App\Tests\WebTestCase;
 
 class MessageControllerTest extends WebTestCase
 {
+    public function testUserCanSendMessage()
+    {
+        $client = $this->createClient();
+        $client->loginUser($this->getUserByUsername('testUser'));
 
+        $u1 = $this->getUserByUsername('testUser1');
+
+        // First thread
+        $crawler = $client->request('GET', '/u/testUser1');
+        $crawler = $client->click($crawler->filter('.kbin-entry-info')->selectLink('Wyślij wiadomość')->link());
+
+        $client->submit(
+            $crawler->selectButton('Gotowe')->form(
+                [
+                    'message[body]' => 'Testowa wiadomość.',
+                ]
+            )
+        );
+
+        $crawler = $client->followRedirect();
+
+        $this->assertSelectorTextContains('.kbin-profile-threads-page', '0 odpowiedzi w wątku z /u/testUser1 - Testowa wiadomość.');
+
+        // Second thread
+        $crawler = $client->request('GET', '/u/testUser1');
+        $crawler = $client->click($crawler->filter('.kbin-entry-info')->selectLink('Wyślij wiadomość')->link());
+
+        $client->submit(
+            $crawler->selectButton('Gotowe')->form(
+                [
+                    'message[body]' => 'Testowa wiadomość2.',
+                ]
+            )
+        );
+
+        $crawler = $client->followRedirect();
+
+        $this->assertSelectorTextContains('.kbin-profile-threads-page', '0 odpowiedzi w wątku z /u/testUser1 - Testowa wiadomość2.');
+
+        $client->loginUser($this->getUserByUsername('testUser1'));
+
+        $crawler = $client->request('GET', '/profil/wiadomosci');
+
+        $this->assertSelectorTextContains('.kbin-profile-threads-page', '0 odpowiedzi w wątku z /u/testUser - Testowa wiadomość.');
+        $this->assertSelectorTextContains('.kbin-profile-threads-page', '0 odpowiedzi w wątku z /u/testUser - Testowa wiadomość2.');
+        $this->assertSelectorTextContains('.kbin-nav .bg-danger', '2');
+
+        // Read message
+        $crawler = $client->click($crawler->filter('.kbin-profile-threads-page')->selectLink('Testowa wiadomość.')->link());
+
+        $this->assertSelectorTextContains('.kbin-nav .bg-danger', '1');
+
+        // Reply
+        $client->submit(
+            $crawler->selectButton('Gotowe')->form(
+                [
+                    'message[body]' => 'Testowa odpowiedź.',
+                ]
+            )
+        );
+
+        $crawler = $client->request('GET', '/profil/wiadomosci');
+
+        $this->assertSelectorTextContains('.kbin-profile-threads-page', '1 odpowiedzi w wątku z /u/testUser - Testowa wiadomość.');
+
+        // Read reply
+        $client->loginUser($this->getUserByUsername('testUser'));
+
+        $crawler = $client->request('GET', '/profil/wiadomosci');
+
+        $this->assertSelectorTextContains('.kbin-profile-threads-page', '1 odpowiedzi w wątku z /u/testUser1 - Testowa wiadomość.');
+        $this->assertSelectorTextContains('.kbin-nav .bg-danger', '1');
+    }
 }
