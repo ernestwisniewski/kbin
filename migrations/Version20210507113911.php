610d5f860e655a70507f665f058653513ff5f66e
Ernest <ernestwisniewski2@gmail.com>
2021-05-07 13:56:09 +0200

    Migrations fix

--- migrations/Version20210507113911.php
+++ migrations/Version20210507113911.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210505153238 extends AbstractMigration
+final class Version20210507113911 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,11 @@ final class Version20210505153238 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE site_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE site (id INT NOT NULL, title VARCHAR(255) NOT NULL, enabled BOOLEAN NOT NULL, registration_open BOOLEAN NOT NULL, PRIMARY KEY(id))');
         $this->addSql('ALTER TABLE entry ADD slug VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE entry ADD ip VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE entry ALTER sticky DROP DEFAULT');
         $this->addSql('ALTER TABLE entry_comment ADD ip VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE post ADD slug VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE post ADD ip VARCHAR(255) DEFAULT NULL');
@@ -32,8 +35,11 @@ final class Version20210505153238 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE site_id_seq CASCADE');
+        $this->addSql('DROP TABLE site');
         $this->addSql('ALTER TABLE entry DROP slug');
         $this->addSql('ALTER TABLE entry DROP ip');
+        $this->addSql('ALTER TABLE entry ALTER sticky SET DEFAULT \'false\'');
         $this->addSql('ALTER TABLE entry_comment DROP ip');
         $this->addSql('ALTER TABLE post DROP slug');
         $this->addSql('ALTER TABLE post DROP ip');
