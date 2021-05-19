61d11dbdcb320fdcf282547f40c7ce93bfa852ab
Ernest <ernestwisniewski2@gmail.com>
2021-05-19 23:41:09 +0200

    Notifications twig

---
+++ migrations/Version20210519195125.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210519195125 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE "user" ADD notify_on_new_entry_comment_reply BOOLEAN NOT NULL');
+        $this->addSql('ALTER TABLE "user" ADD notify_on_new_post_comment_reply BOOLEAN NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE "user" DROP notify_on_new_entry_comment_reply');
+        $this->addSql('ALTER TABLE "user" DROP notify_on_new_post_comment_reply');
+    }
+}
