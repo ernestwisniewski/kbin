0244028c1409751afb2319c94fc959356990a388
Ernest <ernestwisniewski2@gmail.com>
2021-08-27 22:48:14 +0200

    Awesome bot commands

--- src/Command/AwesomeBotFixtures.php
+++ src/Command/AwesomeBotFixtures.php
@@ -12,6 +12,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use DOMElement;
 use Symfony\Component\BrowserKit\HttpBrowser;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
