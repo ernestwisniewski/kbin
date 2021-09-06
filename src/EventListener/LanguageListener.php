811d1ab2c65cf45dc439c4d51372b97984e5c9a1
Ernest <ernestwisniewski2@gmail.com>
2021-09-06 21:34:01 +0200

    Language default

--- src/EventListener/LanguageListener.php
+++ src/EventListener/LanguageListener.php
@@ -6,13 +6,22 @@ use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 class LanguageListener
 {
+    public function __construct(public string $lang)
+    {
+    }
+
     public function onKernelRequest(RequestEvent $event)
     {
-        $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-        $acceptLang = ['pl'];
-        $locale = in_array($lang, $acceptLang) ? $lang : 'en';
-
         $request = $event->getRequest();
+
+        if (!isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+            $request->setLocale($this->lang);
+        }
+
+        $lang       = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+        $acceptLang = ['pl'];
+        $locale     = in_array($lang, $acceptLang) ? $lang : $this->lang;
+
         $request->setLocale($locale);
     }
 }
