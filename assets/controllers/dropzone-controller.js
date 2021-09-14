bb754641fc82d95469217e76b991ca909f83dfcd
Ernest <ernestwisniewski2@gmail.com>
2021-09-14 17:10:26 +0200

    Dropzone init, commets marings

---
+++ assets/controllers/dropzone-controller.js
@@ -0,0 +1,27 @@
+import { Controller } from 'stimulus';
+
+export default class extends Controller {
+    connect() {
+        this.element.addEventListener('dropzone:connect', this._onConnect);
+        this.element.addEventListener('dropzone:change', this._onChange);
+        this.element.addEventListener('dropzone:clear', this._onClear);
+    }
+
+    disconnect() {
+        // You should always remove listeners when the controller is disconnected to avoid side-effects
+        this.element.removeEventListener('dropzone:connect', this._onConnect);
+        this.element.removeEventListener('dropzone:change', this._onChange);
+        this.element.removeEventListener('dropzone:clear', this._onClear);
+    }
+
+    _onConnect(event) {
+    }
+
+    _onChange(event) {
+        // The dropzone just changed
+    }
+
+    _onClear(event) {
+        // The dropzone has just been cleared
+    }
+}
