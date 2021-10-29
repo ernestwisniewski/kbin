798aa12be8581a5a7e36b5d50080558599df489a
Ernest <ernestwisniewski2@gmail.com>
2021-10-29 18:45:55 +0200

    Readme update

---
+++ src/Request/RequestDtoInterface.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace App\Request;
+
+use Symfony\Component\HttpFoundation\Request;
+
+interface RequestDtoInterface
+{
+    public function __construct(Request $request);
+}
