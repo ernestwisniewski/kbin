c7eb4b73301dcb08a07bc444e4eefaf602faf96e
Ernest <ernestwisniewski2@gmail.com>
2021-10-31 10:05:53 +0100

    Entry controllers

---
+++ src/Controller/Entry/EntryDeleteController.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types = 1);
+
+namespace App\Controller\Entry;
+
+use App\Controller\AbstractController;
+use App\Entity\Entry;
+use App\Entity\Magazine;
+use App\Service\EntryManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EntryDeleteController extends AbstractController
+{
+    public function __construct(
+        private EntryManager $manager,
+    ) {
+    }
+
+    /**
+     * @ParamConverter("magazine", options={"mapping": {"magazine_name": "name"}})
+     * @ParamConverter("entry", options={"mapping": {"entry_id": "id"}})
+     *
+     * @IsGranted("ROLE_USER")
+     * @IsGranted("delete", subject="entry")
+     */
+    public function delete(Magazine $magazine, Entry $entry, Request $request): Response
+    {
+        $this->validateCsrf('entry_delete', $request->request->get('token'));
+
+        $this->manager->delete($this->getUserOrThrow(), $entry);
+
+        return $this->redirectToMagazine($magazine);
+    }
+
+    /**
+     * @ParamConverter("magazine", options={"mapping": {"magazine_name": "name"}})
+     * @ParamConverter("entry", options={"mapping": {"entry_id": "id"}})
+     *
+     * @IsGranted("ROLE_USER")
+     * @IsGranted("purge", subject="entry")
+     */
+    public function purge(Magazine $magazine, Entry $entry, Request $request): Response
+    {
+        $this->validateCsrf('entry_purge', $request->request->get('token'));
+
+        $this->manager->purge($this->getUserOrThrow(), $entry);
+
+        return $this->redirectToMagazine($magazine);
+    }
+}
---
+++ src/Controller/Entry/EntryEditController.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types = 1);
+
+namespace App\Controller\Entry;
+
+use App\Controller\AbstractController;
+use App\Entity\Entry;
+use App\Entity\Magazine;
+use App\PageView\EntryPageView;
+use App\Service\EntryManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class EntryEditController extends AbstractController
+{
+    use EntryTemplateTrait;
+    use EntryFormTrait;
+
+    public function __construct(
+        private EntryManager $manager,
+    ) {
+    }
+
+    /**
+     * @ParamConverter("magazine", options={"mapping": {"magazine_name": "name"}})
+     * @ParamConverter("entry", options={"mapping": {"entry_id": "id"}})
+     *
+     * @IsGranted("ROLE_USER")
+     * @IsGranted("edit", subject="entry")
+     */
+    public function __invoke(Magazine $magazine, Entry $entry, Request $request): Response
+    {
+        $dto = $this->manager->createDto($entry);
+
+        $form = $this->createFormByType($dto, (new EntryPageView(1))->resolveType($entry->type));
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$this->isGranted('create_content', $dto->magazine)) {
+                throw new AccessDeniedHttpException();
+            }
+
+            $entry = $this->manager->edit($entry, $dto);
+
+            return $this->redirectToEntry($entry);
+        }
+
+        return $this->render(
+            $this->getTemplateName((new EntryPageView(1))->resolveType($entry->type), true),
+            [
+                'magazine' => $magazine,
+                'entry'    => $entry,
+                'form'     => $form->createView(),
+            ],
+            new Response(null, $form->isSubmitted() && !$form->isValid() ? 422 : 200)
+        );
+    }
+}
