52a271c1cba62d2044580079338da0b6c234da8f
Ernest <ernestwisniewski2@gmail.com>
2021-11-09 01:29:47 +0100

    Magazine component init

---
+++ src/Components/MagazineComponent.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Components;
+
+use App\Entity\Magazine;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('magazine')]
+class MagazineComponent
+{
+    public Magazine $magazine;
+    public bool $showCover = true;
+    public bool $showDescription = true;
+    public bool $showRules = true;
+    public bool $showStats = true;
+}
