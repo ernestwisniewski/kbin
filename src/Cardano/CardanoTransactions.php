fbae0d78d695746f4afa993e2a0730bce1fed804
Ernest <ernestwisniewski2@gmail.com>
2021-11-12 16:40:15 +0100

    Links noopener, noreferrer, Cardano init

---
+++ src/Cardano/CardanoTransactions.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace App\Cardano;
+
+class CardanoTransactions
+{
+    // https://forum.cardano.org/t/how-to-get-started-with-metadata-on-cardano/45111
+
+    public function construct() {}
+
+    public function balance(string $walletId) {}
+}
