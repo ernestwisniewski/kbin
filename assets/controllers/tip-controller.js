b3e5ebef464cb835c10728cbf529a2e2d2fe3f50
Ernest <ernestwisniewski2@gmail.com>
2021-11-14 16:56:01 +0100

    Entry tip styles

--- assets/controllers/tip-controller.js
+++ assets/controllers/tip-controller.js
@@ -8,18 +8,24 @@ export default class extends Controller {
         event.preventDefault();
 
         try {
-            console.log(event.target.closest('a'));
             let response = await fetch(event.target.closest('a').href, {method: 'GET'});
 
             response = await ok(response);
             response = await response.json();
 
             this.containerTarget.innerHTML = response.html;
+
+            this.element.style.backgroundColor = 'transparent';
         } catch (e) {
             alert('Oops, something went wrong.');
             throw e;
         } finally {
             this.loadingValue = false;
         }
+    }
+
+    close() {
+        this.element.style.backgroundColor = null;
+        this.containerTarget.innerHTML = '';
     }
 }
--- src/Entity/Contracts/ContentInterface.php
+++ src/Entity/Contracts/ContentInterface.php
@@ -1,10 +1,13 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace App\Entity\Contracts;
 
 use App\Entity\Magazine;
+use App\Entity\User;
 
 interface ContentInterface
 {
     public function getMagazine(): ?Magazine;
+
+    public function getUser(): ?User;
 }
