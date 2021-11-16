ac9916526220508e515b744e6236ad968f7e7231
Ernest <ernestwisniewski2@gmail.com>
2021-11-16 23:37:45 +0100

    Create wallet controller

--- src/Cardano/CardanoWallet.php
+++ src/Cardano/CardanoWallet.php
@@ -3,7 +3,9 @@
 namespace App\Cardano;
 
 use FurqanSiddiqui\BIP39\BIP39;
+use JetBrains\PhpStorm\ArrayShape;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class CardanoWallet
 {
@@ -11,22 +13,31 @@ class CardanoWallet
     {
     }
 
-    public function create(): string
+    #[ArrayShape(['mnemonic' => "string", 'address' => "string"])] public function create(): array
     {
-        $mnemonic = BIP39::Generate(24)->words;
+        $wallet   = $this->createWallet($mnemonic = BIP39::Generate(15)->words);
+        $walletId = $wallet->toArray()['id'];
 
-        $response = $this->client->request(
+        $addresses = $this->client->request('GET', "$this->cardanoApiUrl/wallets/$walletId/addresses");
+        $address   = $addresses->toArray()[0]['id'];
+
+        $this->client->request('DELETE', "$this->cardanoApiUrl/wallets/$walletId");
+
+        return ['mnemonic' => implode(' ', $mnemonic), 'address' => $address];
+    }
+
+    private function createWallet(array $mnemonic): ResponseInterface
+    {
+        return $this->client->request(
             'POST',
             "$this->cardanoApiUrl/wallets",
             [
                 'json' => [
-                    'name' => 'Kbin Wallet',
+                    'name'              => 'Karabin',
                     'mnemonic_sentence' => $mnemonic,
-                    'passphrase' => 'kbin tips',
+                    'passphrase'        => bin2hex(random_bytes(15))
                 ],
             ]
         );
-
-        return implode(' ', $mnemonic);
     }
 }
