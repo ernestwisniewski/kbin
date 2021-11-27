12723e9142e68eeee8bbec1c6e283927a4c440d0
Ernest <ernestwisniewski2@gmail.com>
2021-11-27 13:53:00 +0100

    Edit screen fix, hidden navbar

---
+++ assets/utils/navbar.js
@@ -0,0 +1,35 @@
+export default class Navbar {
+    constructor() {
+        // https://bootstrap-menu.com/detail-autohide.html
+        this.build();
+
+        document.addEventListener('turbo:load', function () {
+            this.build();
+        });
+    }
+
+    build() {
+        const el_autohide = document.querySelector('.autohide');
+
+        if (el_autohide) {
+            var last_scroll_top = 0;
+            window.addEventListener('scroll', function () {
+                let scroll_top = window.scrollY;
+
+                if (last_scroll_top < 5 || last_scroll_top < 5) {
+                    el_autohide.classList.remove('scrolled-down');
+                    el_autohide.classList.add('scrolled-up');
+                } else if (scroll_top < last_scroll_top) {
+                    el_autohide.classList.remove('scrolled-down');
+                    el_autohide.classList.add('scrolled-up');
+                } else {
+                    el_autohide.classList.remove('scrolled-up');
+                    el_autohide.classList.add('scrolled-down');
+                }
+
+
+                last_scroll_top = scroll_top;
+            });
+        }
+    }
+}
