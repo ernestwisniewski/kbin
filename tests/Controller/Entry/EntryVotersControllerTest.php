90c9852386b82db067a06ae7322c4ca190a12e5b
Ernest <ernestwisniewski2@gmail.com>
2021-12-04 13:25:03 +0100

    Ads block, Voters tests init

--- tests/Controller/Entry/EntryVotersControllerTest.php
+++ tests/Controller/Entry/EntryVotersControllerTest.php
@@ -6,7 +6,7 @@ use App\Tests\WebTestCase;
 
 class EntryVotersControllerTest extends WebTestCase
 {
-    public function testShowEntryVoters()
+    public function testCanSeeSidebarEntryVoters()
     {
         $client = $this->createClient();
         $client->loginUser($this->getUserByUsername('regularUser'));
@@ -21,6 +21,28 @@ class EntryVotersControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/m/polityka/t/'.$entry->getId());
 
-        $this->assertCount(5, $crawler->filter('.kbin-sidebar .kbin-users ul li'));
+        $this->assertCount(6, $crawler->filter('.kbin-sidebar .kbin-voters ul li'));
+    }
+
+    public function testCanSeeEntryVotersPage()
+    {
+        $client = $this->createClient();
+        $client->loginUser($this->getUserByUsername('regularUser'));
+
+        $entry = $this->getEntryByTitle('example');
+        $this->createVote(1, $entry, $this->getUserByUsername('user1'));
+        $this->createVote(1, $entry, $this->getUserByUsername('user2'));
+        $this->createVote(1, $entry, $this->getUserByUsername('user3'));
+        $this->createVote(1, $entry, $this->getUserByUsername('user4'));
+        $this->createVote(2, $entry, $this->getUserByUsername('user5'));
+        $this->createVote(2, $entry, $this->getUserByUsername('user6'));
+
+        $crawler = $client->request('GET', '/m/polityka/t/'.$entry->getId());
+
+        $this->assertCount(6, $crawler->filter('.kbin-sidebar .kbin-voters ul li'));
+
+        $crawler = $client->click($crawler->filter('.kbin-sidebar .kbin-voters ul li')->last()->filter('a')->link());
+
+        $this->assertCount(6, $crawler->filter('.kbin-main .kbin-voters .card'));
     }
 }
