2b044948c64d2f9f19e1b3b598f3db02b17ff619
Ernest <ernestwisniewski2@gmail.com>
2021-12-07 00:33:46 +0100

    Delete post, entry test, delete account confirmation

--- tests/Controller/Post/PostDeleteControllerTest.php
+++ tests/Controller/Post/PostDeleteControllerTest.php
@@ -2,11 +2,13 @@
 
 namespace App\Tests\Controller\Post;
 
+use App\Repository\PostCommentRepository;
+use App\Repository\PostRepository;
 use App\Tests\WebTestCase;
 
 class PostDeleteControllerTest extends WebTestCase
 {
-    public function testCanDeletePost()
+    public function testAuthorCanDeletePost()
     {
         $client = $this->createClient();
         $client->loginUser($user = $this->getUserByUsername('regularUser'));
@@ -40,5 +42,49 @@ class PostDeleteControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
 
         $this->assertCount(0, $crawler->filter('.kbin-post'));
+
+        $repository = static::getContainer()->get(PostRepository::class);
+        $posts    = $repository->findAll();
+        $this->assertCount(1, $posts);
+
+        $repository = static::getContainer()->get(PostCommentRepository::class);
+        $comments   = $repository->findAll();
+        $this->assertCount(3, $comments);
+    }
+
+    public function testAdminCanPurgePost()
+    {
+        $client = $this->createClient();
+        $client->loginUser($user = $this->getUserByUsername('regularUser'));
+
+        $user1 = $this->getUserByUsername('regularUser');
+        $user2 = $this->getUserByUsername('regularUser2');
+
+        $post = $this->createPost('post test', null, $user1);
+
+        $comment1 = $this->createPostComment('test', $post, $user);
+        $comment2 = $this->createPostComment('test2', $post, $user1);
+        $comment3 = $this->createPostComment('test3', $post, $user2);
+
+        $this->createVote(1, $post, $user2);
+        $this->createVote(1, $comment1, $user2);
+        $this->createVote(1, $comment2, $user2);
+        $this->createVote(1, $comment3, $user);
+
+        $client->loginUser($admin = $this->getUserByUsername('admin', true));
+
+        $crawler = $client->request('GET', "/m/polityka/w/{$post->getId()}");
+
+        $client->submit($crawler->filter('.kbin-post-main')->selectButton('wyczyść')->form());
+
+        $crawler = $client->followRedirect();
+
+        $repository = static::getContainer()->get(PostRepository::class);
+        $posts    = $repository->findAll();
+        $this->assertCount(0, $posts);
+
+        $repository = static::getContainer()->get(PostCommentRepository::class);
+        $comments   = $repository->findAll();
+        $this->assertCount(0, $comments);
     }
 }
