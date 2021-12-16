f2857f9594906fb9345e341ed427ed091cb8e39e
Ernest <ernestwisniewski2@gmail.com>
2021-12-16 20:23:10 +0100

    Magazine block, Entry tip tests, js components lazy loading

--- tests/Controller/Magazine/MagazineBlockControllerTest.php
+++ tests/Controller/Magazine/MagazineBlockControllerTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Controller\Magazine;
 
 use App\Service\MagazineManager;
 use App\Tests\WebTestCase;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MagazineBlockControllerTest extends WebTestCase
 {
@@ -41,5 +42,44 @@ class MagazineBlockControllerTest extends WebTestCase
 
         $this->assertStringContainsString('kbin-block--active', $crawler->filter('.kbin-sidebar .kbin-magazine .kbin-magazine-block')->attr('class'));
         $this->assertSelectorTextContains('.kbin-magazine-header .kbin-sub', '1');
+    }
+
+    public function testXmlUserCanBlockAndUnblockMagazine() // @todo
+    {
+        $client = $this->createClient();
+        $client->loginUser($user = $this->getUserByUsername('regularUser'));
+
+        $user2 = $this->getUserByUsername('regularUser2');
+        $user2->cardanoWalletAddress('abc');
+
+        $manager = static::getContainer()->get(EntityManagerInterface::class);
+        $manager->persist($user2);
+        $manager->flush();
+
+        $magazine = $this->getMagazineByName('polityka', $user2);
+        $entry    = $this->getEntryByTitle('treść 2', null, null, $magazine, $user2);
+
+        $id = $entry->getId();
+        $client->request('GET', "/m/polityka/t/$id/-/");
+
+        $crawler = $client->followRedirect();
+
+        $client->setServerParameter('HTTP_X-Requested-With', 'XMLHttpRequest');
+        $client->submit(
+            $crawler->filter('.kbin-sidebar .kbin-magazine .kbin-magazine-block ')->selectButton('')->form()
+        );
+
+        $this->assertStringContainsString('{"isBlocked":true}', $client->getResponse()->getContent());
+
+        $client->request('GET', "/m/polityka/t/$id/-/");
+
+        $crawler = $client->followRedirect();
+
+        $client->setServerParameter('HTTP_X-Requested-With', 'XMLHttpRequest');
+        $client->submit(
+            $crawler->filter('.kbin-sidebar .kbin-magazine .kbin-magazine-block ')->selectButton('')->form()
+        );
+
+        $this->assertStringContainsString('{"isBlocked":false}', $client->getResponse()->getContent());
     }
 }
