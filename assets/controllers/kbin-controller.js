0dc89e0423daf2f1cc98b1bb3b5318640ccdec46
Ernest <ernestwisniewski2@gmail.com>
2021-12-21 19:04:37 +0100

    Cookies info bar, comments, posts paddings, guest theme

--- assets/controllers/kbin-controller.js
+++ assets/controllers/kbin-controller.js
@@ -4,21 +4,45 @@ import Cookies from 'js-cookie';
 
 export default class extends Controller {
     connect() {
+        if (window.KBIN_LOGGED_IN) {
+            return true;
+        }
+
+        if (!document.body.classList.contains('kbin-light') && !document.body.classList.contains('kbin-dark')) {
+            return true
+        }
+
+        if (Cookies.get('theme') === 'kbin-dark') {
+            document.body.classList.add('kbin-dark');
+        }
     }
 
     async toggleTheme(e) {
         e.preventDefault();
 
-        try {
-            let response = await fetch(e.target.href, {method: 'POST'});
+        if (window.KBIN_LOGGED_IN) {
+            try {
+                let response = await fetch(e.target.href, {method: 'POST'});
 
-            response = await ok(response);
-            response = await response.json();
+                response = await ok(response);
+                response = await response.json();
 
+                document.body.classList.toggle('kbin-dark');
+            } catch (e) {
+                document.body.classList.toggle('kbin-dark');
+            } finally {
+            }
+        } else {
+            if (!Cookies.get('theme')) {
+                document.body.classList.add('kbin-dark');
+                Cookies.set('theme', 'kbin-dark');
+                return true;
+            }
+
+            Cookies.set('theme', document.body.classList.contains('kbin-dark') ? 'kbin-light' : 'kbin-dark');
             document.body.classList.toggle('kbin-dark');
-        } catch (e) {
-            document.body.classList.toggle('kbin-dark');
-        } finally {
+
+            alert('Zaloguj się, aby uniknąć efektu miagania.')
         }
     }
 }
