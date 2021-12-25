632db56755133f7d2d04d8200bc99ecd248d949b
Ernest <ernestwisniewski2@gmail.com>
2021-12-25 11:51:32 +0100

    Entry comment delete thumbs

--- assets/controllers/media-controller.js
+++ assets/controllers/media-controller.js
@@ -1,7 +1,8 @@
 import {Controller} from '@hotwired/stimulus';
+import {fetch, ok} from "../utils/http";
 
 export default class extends Controller {
-    static targets = ['modalButton', 'deleteButton', 'fileInput', 'canvas', 'close']
+    static targets = ['modalButton', 'deleteButton', 'fileInput', 'canvas', 'thumb', 'close']
 
     add(e) {
         let reader = new FileReader();
@@ -25,16 +26,35 @@ export default class extends Controller {
         this.closeTarget.click();
     }
 
-    async delete() {
+    async delete(event) {
+        event.preventDefault();
+
         if (this.hasCanvasTarget) {
             this.canvasTarget.getContext('2d').clearRect(0, 0, 0, 0);
 
             this.canvasTarget.classList.add('d-none');
             this.deleteButtonTarget.classList.add('d-none');
-
             this.modalButtonTarget.classList.remove('d-none');
 
             this.fileInputTarget.value = '';
+
+            return;
+        }
+
+        try {
+            let response = await fetch(event.params.url, {method: 'POST'});
+
+            response = await ok(response);
+            response = await response.json();
+
+            this.thumbTarget.classList.add('d-none');
+            this.deleteButtonTarget.classList.add('d-none');
+            this.modalButtonTarget.classList.remove('d-none');
+
+            this.fileInputTarget.value = '';
+        } catch (e) {
+            alert('Oops, something went wrong.');
+            throw e;
         }
     }
 }
---
+++ src/Controller/Entry/Comment/CommentDeleteImageController.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller\Entry\Comment;
+
+use App\Controller\AbstractController;
+use App\Entity\Entry;
+use App\Entity\EntryComment;
+use App\Entity\Magazine;
+use App\Service\EntryCommentManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CommentDeleteImageController extends AbstractController
+{
+    public function __construct(
+        private EntryCommentManager $manager
+    ) {
+    }
+
+    /**
+     * @ParamConverter("magazine", options={"mapping": {"magazine_name": "name"}})
+     * @ParamConverter("entry", options={"mapping": {"entry_id": "id"}})
+     * @ParamConverter("comment", options={"mapping": {"comment_id": "id"}})
+     *
+     * @IsGranted("ROLE_USER")
+     * @IsGranted("delete", subject="comment")
+     */
+    public function __invoke(Magazine $magazine, Entry $entry, EntryComment $comment, Request $request): Response
+    {
+        $this->manager->detachImage($comment);
+
+        if ($request->isXmlHttpRequest()) {
+            return new JsonResponse(
+                [
+                    'success' => true,
+                ]
+            );
+        }
+
+        return $this->redirectToEntry($entry);
+    }
+}
