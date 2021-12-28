749f470cc49a9d070a77ffa97d20cb479540addf
Ernest <ernestwisniewski2@gmail.com>
2021-12-28 15:55:57 +0100

    Magazine trash init

---
+++ src/Controller/Magazine/Panel/MagazineTrashController.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller\Magazine\Panel;
+
+use App\Controller\AbstractController;
+use App\Entity\Magazine;
+use App\Service\BadgeManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MagazineTrashController extends AbstractController
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * @IsGranted("ROLE_USER")
+     * @IsGranted("moderate", subject="magazine")
+     */
+    public function __invoke(Magazine $magazine, BadgeManager $manager, Request $request): Response
+    {
+        return $this->render(
+            'magazine/panel/trash.html.twig',
+            [
+                'magazine' => $magazine,
+            ]
+        );
+    }
+}
+
