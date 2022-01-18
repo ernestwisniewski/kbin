69ac9a8b116cde217381568b1f57cdcc1e3267c6
Ernest <ernestwisniewski2@gmail.com>
2022-01-18 16:42:46 +0100

    User, magazine panel tabs and structure

--- src/Controller/User/Profile/UserReportsController.php
+++ src/Controller/User/Profile/UserReportsController.php
@@ -1,21 +1,23 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace App\Controller\User\Profile;
 
 use App\Controller\AbstractController;
-use App\Repository\NotificationRepository;
-use App\Service\NotificationManager;
+use App\Repository\MagazineRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserReportsController extends AbstractController
 {
+    const MODERATED = 'moderated';
+
     /**
      * @IsGranted("ROLE_USER")
      */
-    public function __invoke(NotificationRepository $repository, Request $request): Response
+    public function __invoke(MagazineRepository $repository, Request $request): Response
     {
+
         return $this->render(
             'user/profile/reports.html.twig',
             [
