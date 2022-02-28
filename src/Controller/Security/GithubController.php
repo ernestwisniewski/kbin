e20240e3e41b6b78c3ccf36de930ecb7bd7b0c89
Ernest <ernestwisniewski2@gmail.com>
2022-03-01 00:04:35 +0100

    Oauth init

---
+++ src/Controller/Security/GithubController.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller\Security;
+
+use App\Controller\AbstractController;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class GithubController extends AbstractController
+{
+    public function connect(ClientRegistry $clientRegistry): Response
+    {
+        return $clientRegistry
+            ->getClient('github')
+            ->redirect([
+                'public_profile',
+                'email',
+            ]);
+    }
+
+    public function verify(Request $request, ClientRegistry $client)
+    {
+    }
+}
