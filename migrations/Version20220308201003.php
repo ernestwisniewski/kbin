9a94cd37d4a50d1fa712aab5e8469df7bd38334e
Ernest <ernestwisniewski2@gmail.com>
2022-03-08 21:10:32 +0100

    Settings - featured magazines bar migrtions

---
+++ migrations/Version20220308201003.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220308201003 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE "user" ADD featured_magazines TEXT DEFAULT NULL');
+        $this->addSql('COMMENT ON COLUMN "user".featured_magazines IS \'(DC2Type:array)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE "user" DROP featured_magazines');
+    }
+}
