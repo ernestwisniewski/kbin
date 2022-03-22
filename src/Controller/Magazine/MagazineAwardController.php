fde14b93c664f43599ae9f309faabda6d85501fb
Ernest <ernestwisniewski2@gmail.com>
2022-03-23 00:09:31 +0100

    Magazine awards init

---
+++ src/Controller/Magazine/MagazineAwardController.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller\Magazine;
+
+use App\Controller\AbstractController;
+use App\Entity\Magazine;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MagazineAwardController extends AbstractController
+{
+    public function __invoke(Magazine $magazine, ?string $category, Request $request): Response
+    {
+        return $this->render(
+            'award/list_all.html.twig',
+            [
+                'magazine' => $magazine,
+                'category' => $category,
+                'types'    => [],
+            ]
+        );
+    }
+}
