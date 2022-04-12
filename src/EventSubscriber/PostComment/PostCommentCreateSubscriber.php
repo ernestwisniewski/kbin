64d2ae549b37e1a1d36489a55ae4372b9bc56a98
Ernest <ernestwisniewski2@gmail.com>
2022-04-12 23:18:36 +0200

    Post comments preview cache, create post redirect

--- src/EventSubscriber/PostComment/PostCommentCreateSubscriber.php
+++ src/EventSubscriber/PostComment/PostCommentCreateSubscriber.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace App\EventSubscriber\PostComment;
 
@@ -6,10 +6,11 @@ use App\Event\PostComment\PostCommentCreatedEvent;
 use App\Message\Notification\PostCommentCreatedNotificationMessage;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class PostCommentCreateSubscriber implements EventSubscriberInterface
 {
-    public function __construct(private MessageBusInterface $bus)
+    public function __construct(private CacheInterface $cache, private MessageBusInterface $bus)
     {
     }
 
@@ -22,7 +23,7 @@ class PostCommentCreateSubscriber implements EventSubscriberInterface
 
     public function onPostCommentCreated(PostCommentCreatedEvent $event)
     {
+        $this->cache->delete('comments_preview_post_'.$event->comment->post->getId());
         $this->bus->dispatch(new PostCommentCreatedNotificationMessage($event->comment->getId()));
-
     }
 }
--- src/EventSubscriber/PostComment/PostCommentDeleteSubscriber.php
+++ src/EventSubscriber/PostComment/PostCommentDeleteSubscriber.php
@@ -7,10 +7,11 @@ use App\Event\PostComment\PostCommentDeletedEvent;
 use App\Message\Notification\PostCommentDeletedNotificationMessage;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class PostCommentDeleteSubscriber implements EventSubscriberInterface
 {
-    public function __construct(private MessageBusInterface $bus)
+    public function __construct(private CacheInterface $cache, private MessageBusInterface $bus)
     {
     }
 
@@ -24,11 +25,13 @@ class PostCommentDeleteSubscriber implements EventSubscriberInterface
 
     public function onPostCommentDeleted(PostCommentDeletedEvent $event): void
     {
+        $this->cache->delete('comments_preview_post_'.$event->comment->post->getId());
         $this->bus->dispatch(new PostCommentDeletedNotificationMessage($event->comment->getId()));
     }
 
     public function onPostCommentBeforePurge(PostCommentBeforePurgeEvent $event): void
     {
+        $this->cache->delete('comments_preview_post_'.$event->comment->post->getId());
         $this->bus->dispatch(new PostCommentDeletedNotificationMessage($event->comment->getId()));
     }
 }
