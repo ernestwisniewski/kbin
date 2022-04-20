9b667a5cc5835907b8bbaad7d964c7e34dea505c
Ernest <ernestwisniewski2@gmail.com>
2022-04-20 07:44:21 +0200

    Animaten stats numers, update dependencies

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -2,7 +2,8 @@ import {startStimulusApp} from '@symfony/stimulus-bridge';
 import Reveal from '@symfony/stimulus-bridge/lazy-controller-loader?lazy=true!stimulus-reveal-controller';
 import Clipboard from '@symfony/stimulus-bridge/lazy-controller-loader?lazy=true!stimulus-clipboard';
 import ScrollProgress from '@symfony/stimulus-bridge/lazy-controller-loader?lazy=true!stimulus-scroll-progress';
-import CharacterCounter from 'stimulus-character-counter';
+import CharacterCounter from '@symfony/stimulus-bridge/lazy-controller-loader?lazy=true!stimulus-character-counter';
+import AnimatedNumber from '@symfony/stimulus-bridge/lazy-controller-loader?lazy=true!stimulus-animated-number';
 import Timeago from "./utils/timeago";
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
@@ -17,5 +18,6 @@ app.register('clipboard', Clipboard);
 app.register('scroll-progress', ScrollProgress);
 app.register('timeago', Timeago);
 app.register('character-counter', CharacterCounter);
+app.register('animated-number', AnimatedNumber)
 
 export {app};
