9adb34bab3848c92ce10dd8525e1d49b0a930014
Ernest <ernestwisniewski2@gmail.com>
2022-05-08 00:34:26 +0200

    Ajax tests

--- tests/Controller/AjaxControllerTest.php
+++ tests/Controller/AjaxControllerTest.php
@@ -2,12 +2,6 @@
 
 namespace App\Tests\Controller;
 
-use App\DTO\MagazineBanDto;
-use App\Service\EntryCommentManager;
-use App\Service\EntryManager;
-use App\Service\MagazineManager;
-use App\Service\PostCommentManager;
-use App\Service\PostManager;
 use App\Tests\WebTestCase;
 
 class AjaxControllerTest extends WebTestCase
@@ -18,7 +12,7 @@ class AjaxControllerTest extends WebTestCase
 
         $entry = $this->getEntryByTitle('Lorem ipsum', null, 'dolor sit amet');
 
-        $client->jsonRequest('GET', '/ajax/fetch_entry/' . $entry->getId());
+        $client->jsonRequest('GET', '/ajax/fetch_entry/'.$entry->getId());
 
         $this->assertStringContainsString('Lorem ipsum', $client->getResponse()->getContent());
     }
@@ -29,8 +23,81 @@ class AjaxControllerTest extends WebTestCase
 
         $entry = $this->getEntryByTitle('Lorem ipsum', 'https://youtube.com');
 
-        $client->jsonRequest('GET', '/ajax/fetch_entry/' . $entry->getId());
+        $client->jsonRequest('GET', '/ajax/fetch_entry/'.$entry->getId());
 
         $this->assertStringContainsString('Lorem ipsum', $client->getResponse()->getContent());
+    }
+
+    public function testFetchDuplicates()
+    {
+        $client = $this->createClient();
+
+        $client->loginUser($this->getUserByUsername('testUser'));
+
+        $client->jsonRequest('POST', '/ajax/fetch_duplicates', [
+            'url' => 'https://karab.in',
+        ]);
+
+        $this->assertStringContainsString('"total":0', $client->getResponse()->getContent());
+
+        $this->getEntryByTitle('Lorem ipsum', 'https://karab.in');
+
+        $client->jsonRequest('POST', '/ajax/fetch_duplicates', [
+            'url' => 'https://karab.in',
+        ]);
+
+        $this->assertStringContainsString('"total":1', $client->getResponse()->getContent());
+    }
+
+    public function testFetchEntryComment()
+    {
+        $client = $this->createClient();
+
+        $client->loginUser($this->getUserByUsername('regularUser'));
+
+        $comment = $this->createEntryComment('Lorem ipsum');
+
+        $client->jsonRequest('GET', '/ajax/fetch_entry_comment/'.$comment->getId());
+
+        $this->assertStringContainsString('kbin-comment', $client->getResponse()->getContent());
+        $this->assertStringContainsString('Lorem ipsum', $client->getResponse()->getContent());
+    }
+
+    public function testFetchPost() {
+        $client = $this->createClient();
+
+        $client->loginUser($this->getUserByUsername('regularUser'));
+
+        $post = $this->createPost('Lorem ipsum');
+
+        $client->jsonRequest('GET', '/ajax/fetch_post/'.$post->getId());
+
+        $this->assertStringContainsString('kbin-post', $client->getResponse()->getContent());
+        $this->assertStringContainsString('Lorem ipsum', $client->getResponse()->getContent());
+    }
+
+    public function testFetchPostComment() {
+        $client = $this->createClient();
+
+        $client->loginUser($this->getUserByUsername('regularUser'));
+
+        $post = $this->createPost('Lorem ipsum');
+
+        $comment = $this->createPostComment('Lorem ipsum comment', $post);
+
+        $client->jsonRequest('GET', '/ajax/fetch_post_comment/'.$comment->getId());
+
+        $this->assertStringContainsString('kbin-comment', $client->getResponse()->getContent());
+        $this->assertStringContainsString('Lorem ipsum comment', $client->getResponse()->getContent());
+    }
+
+    public function testFetchUserPopup() {
+        $client = $this->createClient();
+
+        $client->loginUser($this->getUserByUsername('regularUser'));
+
+        $client->jsonRequest('GET', '/ajax/fetch_user_popup/regularUser');
+
+        $this->assertStringContainsString('kbin-user-popup', $client->getResponse()->getContent());
     }
 }
