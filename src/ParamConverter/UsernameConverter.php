91b0ed4eee6584e6d70e3a2a7fc28a27109cdb91
Ernest <ernestwisniewski2@gmail.com>
2022-06-08 22:15:01 +0200

    Case insensitive magazine name, username

---
+++ src/ParamConverter/UsernameConverter.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace App\ParamConverter;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use JetBrains\PhpStorm\Pure;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class UsernameConverter implements ParamConverterInterface
+{
+    public function __construct(private UserRepository $repository)
+    {
+    }
+
+    public function apply(Request $request, ParamConverter $configuration)
+    {
+        $username = $request->attributes->get('username') ?? $request->attributes->get('user');
+
+        // @todo case-insensitive
+        if (!$user = $this->repository->findOneByUsername($username)) {
+            throw new NotFoundHttpException();
+        }
+
+        $request->attributes->set($configuration->getName(), $user);
+    }
+
+    #[Pure] public function supports(ParamConverter $configuration): bool
+    {
+        if (null === $configuration->getClass()) {
+            return false;
+        }
+
+        if ($configuration->getClass() !== User::class) {
+            return false;
+        }
+
+        return true;
+    }
+}
