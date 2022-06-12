15c5dfd36990c387268eb9bd548b417edbfe7567
Ernest <ernestwisniewski2@gmail.com>
2022-06-12 16:36:34 +0200

    Posts endpoint temporary fix

--- src/EventSubscriber/Api/BestCommentsFixApiSubscriber.php
+++ src/EventSubscriber/Api/BestCommentsFixApiSubscriber.php
@@ -3,25 +3,13 @@
 namespace App\EventSubscriber\Api;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
-use App\Factory\ImageFactory;
-use App\Factory\PostCommentFactory;
-use App\Factory\UserFactory;
-use App\Repository\PostRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-// @todo temporary fix
+// @todo serialization temporary fix
 final class BestCommentsFixApiSubscriber implements EventSubscriberInterface
 {
-    public function __construct(
-        private PostRepository $repository,
-        private PostCommentFactory $commentFactory,
-        private UserFactory $userFactory,
-        private ImageFactory $imageFactory
-    ) {
-    }
-
     public static function getSubscribedEvents()
     {
         return [
@@ -35,11 +23,10 @@ final class BestCommentsFixApiSubscriber implements EventSubscriberInterface
             return;
         }
 
-        $result = $event->getControllerResult();
+        $results = json_decode($event->getControllerResult(), true);
 
-        $results = json_decode($result, true);
         foreach ($results['hydra:member'] as $index => $member) {
-            $results['hydra:member'][$index]['bestComments'] = array_values($results['hydra:member'][$index]['bestComments']);
+            $results['hydra:member'][$index]['bestComments'] = array_values($member['bestComments']);
         }
 
         $event->setControllerResult(json_encode($results));
