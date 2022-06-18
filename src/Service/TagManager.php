e8833c9bbd1e060a3616a16b4759764d3a6cfde3
Ernest <ernestwisniewski2@gmail.com>
2022-06-18 21:32:40 +0200

    Tag manager fix

--- src/Service/TagManager.php
+++ src/Service/TagManager.php
@@ -9,7 +9,7 @@ class TagManager
         preg_match_all("/#(\w+)/", $val, $matches);
 
         $result = array_unique($matches[1]);
-        $result = array_map(fn($tag) => trim($tag), $result);
+        $result = array_map(fn($tag) => strtolower(trim($tag)), $result);
         $result = array_diff($result, [$magazineName]);
 
         return  count($result) ? array_values($result) : null;
