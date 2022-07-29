532acaa99d60f567e550b8508266fb9e2b509f06
Ernest <ernestwisniewski2@gmail.com>
2022-07-29 23:23:49 +0200

    Lazy preview user settings

--- assets/controllers/user-data-controller.js
+++ assets/controllers/user-data-controller.js
@@ -16,16 +16,24 @@ export default class extends Controller {
         Cookies.set('user_option_' + e.target.dataset.userDataValue, e.target.checked);
 
         if (e.target.dataset.userDataValue === 'auto_embed') {
-            this.togglePreview();
             location.reload();
+            return;
+            this.togglePreview();
         }
     }
 
     async togglePreview() {
         const sleep = ms => new Promise(r => setTimeout(r, Math.random() * (500 - 2000) + 500));
 
+        let observer = new IntersectionObserver(function (entries, observer) {
+            if (entries[0].isIntersecting === true) {
+                entries[0].target.click();
+                observer.unobserve(entries[0].target);
+            }
+        }, { threshold: [0] });
+
         for (const el of document.querySelectorAll('.kbin-preview')) {
-            el.click();
+            observer.observe(el);
             await sleep();
         }
     }
