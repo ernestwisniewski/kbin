5ea6da36643d2bb300eb55cd1a79d7bf56e79e73
Ernest <ernestwisniewski2@gmail.com>
2022-07-29 19:41:49 +0200

    Auto preview

--- assets/controllers/user-data-controller.js
+++ assets/controllers/user-data-controller.js
@@ -2,14 +2,29 @@ import {Controller} from '@hotwired/stimulus';
 import Cookies from 'js-cookie';
 
 export default class extends Controller {
-    static targets = ['autorefresh', 'notifications', 'federation'];
-    static values = {
-        autorefresh: Boolean,
-        notifications: Boolean,
-        federation: Boolean
+    static targets = ['auto_refresh', 'auto_embed', 'notifications', 'federation'];
+
+    connect() {
+        if (Cookies.get('user_option_auto_embed') === 'true') {
+            document.addEventListener('turbo:load', () => {
+                this.togglePreview()
+                const sleep = ms => new Promise(r => setTimeout(r, Math.random() * (500 - 2000)) + 500);
+            });
+        }
     }
 
     toggle(e) {
-        Cookies.set(e.target.dataset.userDataValue, e.target.checked);
+        Cookies.set('user_option_' + e.target.dataset.userDataValue, e.target.checked);
+
+        if (e.target.dataset.userDataValue === 'auto_embed') {
+            this.togglePreview();
+            location.reload();
+        }
+    }
+
+    togglePreview() {
+        document.querySelectorAll('.kbin-preview').forEach(el => {
+            el.click();
+        });
     }
 }
