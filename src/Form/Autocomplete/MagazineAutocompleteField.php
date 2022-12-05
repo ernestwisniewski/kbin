98aca10e867b042659c09406b4ec922b0c39c643
Ernest Wiśniewski <ernestwisniewski2@gmail.com>
2022-12-05 23:07:49 +0100

    Magazine form autocomplete query, update dependencies

--- src/Form/Autocomplete/MagazineAutocompleteField.php
+++ src/Form/Autocomplete/MagazineAutocompleteField.php
@@ -4,6 +4,8 @@ namespace App\Form\Autocomplete;
 
 use App\Entity\Magazine;
 use App\Repository\MagazineRepository;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\UX\Autocomplete\Form\AsEntityAutocompleteField;
@@ -15,12 +17,16 @@ class MagazineAutocompleteField extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'class'         => Magazine::class,
-            'choice_label'  => 'name',
-            'query_builder' => function (MagazineRepository $magazineRepository) {
-                return $magazineRepository->createQueryBuilder('m');
-            },
-            //'security' => 'ROLE_SOMETHING',
+            'class' => Magazine::class,
+            'choice_label' => 'name',
+            'filter_query' => function(QueryBuilder $qb, string $query) {
+                if (!$query) {
+                    return;
+                }
+
+                $qb->andWhere('entity.name LIKE :filter OR entity.title LIKE :filter')
+                    ->setParameter('filter', '%'.$query.'%');
+            }
         ]);
     }
 
