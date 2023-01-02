0171313793219e4d79efe96accd5bc55cef779d4
Ernest Wiśniewski <ernestwisniewski2@gmail.com>
2023-01-02 18:14:44 +0100

    Auto add comments fix

--- assets/controllers/comment-list-controller.js
+++ assets/controllers/comment-list-controller.js
@@ -1,7 +1,8 @@
-import {ApplicationController} from 'stimulus-use'
+import { ApplicationController } from 'stimulus-use'
 import CommentFactory from "../utils/comment-factory";
 import router from "../utils/routing";
-import {fetch, ok} from "../utils/http";
+import { fetch, ok } from "../utils/http";
+import Cookies from 'js-cookie';
 
 export default class extends ApplicationController {
     static values = {
@@ -21,37 +22,39 @@ export default class extends ApplicationController {
             return;
         }
 
-        try {
-            const url = router().generate(route, {'id': notification.detail.id});
+        if (Cookies.get('user_option_auto_refresh') === 'true') {
+            try {
+                const url = router().generate(route, { 'id': notification.detail.id });
 
-            let response = await fetch(url, {method: 'GET'});
+                let response = await fetch(url, { method: 'GET' });
 
-            response = await ok(response);
-            response = await response.json();
+                response = await ok(response);
+                response = await response.json();
 
-            const html = response.html;
+                const html = response.html;
 
-            let div = document.createElement('div');
-            div.innerHTML = html;
+                let div = document.createElement('div');
+                div.innerHTML = html;
 
-            let parent = div.firstElementChild.dataset.commentParentValue;
-            parent = this.element.querySelector(`[data-comment-id-value='${parent}']`);
+                let parent = div.firstElementChild.dataset.commentParentValue;
+                parent = this.element.querySelector(`[data-comment-id-value='${parent}']`);
 
-            const existed = div.firstElementChild.dataset.commentIdValue;
-            if (this.element.querySelector(`[data-comment-id-value='${existed}']`)) {
-                return;
+                const existed = div.firstElementChild.dataset.commentIdValue;
+                if (this.element.querySelector(`[data-comment-id-value='${existed}']`)) {
+                    return;
+                }
+
+                if (parent) {
+                    notification.detail.op === 'EntryCommentCreatedNotification'
+                        ? CommentFactory.create(html, parent)
+                        : this.element.append(div.firstElementChild);
+                } else {
+                    notification.detail.op === 'EntryCommentCreatedNotification'
+                        ? this.element.prepend(div.firstElementChild)
+                        : this.element.append(div.firstElementChild);
+                }
+            } catch (e) {
             }
-
-            if (parent) {
-                notification.detail.op === 'EntryCommentCreatedNotification'
-                    ? CommentFactory.create(html, parent)
-                    : this.element.append(div.firstElementChild);
-            } else {
-                notification.detail.op === 'EntryCommentCreatedNotification'
-                    ? this.element.prepend(div.firstElementChild)
-                    : this.element.append(div.firstElementChild);
-            }
-        } catch (e) {
         }
     }
 }
