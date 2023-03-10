4825e92fbb6c995de6585f9969d03177e9c6e274
Ernest Wiśniewski <ernestwisniewski2@gmail.com>
2023-03-10 18:16:51 +0100

    Theme settings init

--- src/Controller/User/ThemeSettingsController.php
+++ src/Controller/User/ThemeSettingsController.php
@@ -14,16 +14,23 @@ class ThemeSettingsController extends AbstractController
     public const ENTRIES_VIEW = 'entries_view';
     public const ENTRY_COMMENTS_VIEW = 'entry_comments_view';
     public const POST_COMMENTS_VIEW = 'post_comments_view';
+    public const KBIN_THEME = 'kbin_theme';
 
     public const CLASSIC = 'classic';
     public const CHAT = 'chat';
     public const TREE = 'tree';
     public const COMPACT = 'compact';
+    public const LIGHT = 'light';
+    public const DARK = 'dark';
+    public const SOLARIZED_LIGHT = 'solarized-light';
+    public const SOLARIZED_DARK = 'solarized-dark';
+
 
     public const KEYS = [
         self::ENTRIES_VIEW,
         self::ENTRY_COMMENTS_VIEW,
         self::POST_COMMENTS_VIEW,
+        self::KBIN_THEME,
     ];
 
     public const VALUES = [
@@ -31,6 +38,10 @@ class ThemeSettingsController extends AbstractController
         self::CHAT,
         self::TREE,
         self::COMPACT,
+        self::LIGHT,
+        self::DARK,
+        self::SOLARIZED_LIGHT,
+        self::SOLARIZED_DARK,
     ];
 
     public function __invoke(string $key, string $value, Request $request): Response
