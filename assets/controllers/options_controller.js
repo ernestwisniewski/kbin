8f976cd241624b3d723a8e5ff1a04a6c394769b9
Ernest Wiśniewski <ernestwisniewski2@gmail.com>
2023-03-20 15:27:06 +0100

    Options active class

--- assets/controllers/options_controller.js
+++ assets/controllers/options_controller.js
@@ -13,8 +13,13 @@ export default class extends Controller {
         if (false === this.initialValue || this.federationTarget.style.display === 'none') {
             this.settingsTarget.style.display = 'none';
             this.federationTarget.style.display = 'block';
+
+            this.removeActiveClass();
+            event.target.classList.add('active');
+
             this.initialValue = true;
         } else {
+            event.target.classList.remove('active');
             this.federationTarget.style.display = 'none';
         }
     }
@@ -25,9 +30,21 @@ export default class extends Controller {
         if (false === this.initialValue || this.settingsTarget.style.display === 'none') {
             this.federationTarget.style.display = 'none';
             this.settingsTarget.style.display = 'block';
+
+            this.removeActiveClass();
+            event.target.classList.add('active');
+
             this.initialValue = true;
         } else {
+            event.target.classList.remove('active');
             this.settingsTarget.style.display = 'none';
         }
+    }
+
+    removeActiveClass() {
+        this.element.querySelector('.options')
+            .querySelectorAll('.active').forEach(element => {
+            element.classList.remove('active');
+        });
     }
 }
