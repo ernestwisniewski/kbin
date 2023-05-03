5b54d06f0d8e5341cde504a19be8c4e0e7ae6018
Ernest Wiśniewski <ernestwisniewski2@gmail.com>
2023-05-03 19:24:58 +0200

    Http timeout, instance endpoint cache

--- src/Controller/ActivityPub/InstanceController.php
+++ src/Controller/ActivityPub/InstanceController.php
@@ -7,11 +7,17 @@ namespace App\Controller\ActivityPub;
 use App\Factory\ActivityPub\InstanceFactory;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class InstanceController
 {
-    public function __invoke(Request $request, InstanceFactory $instanceFactory): JsonResponse
+    public function __invoke(Request $request, InstanceFactory $instanceFactory, CacheInterface $cache): JsonResponse
     {
-        return new JsonResponse($instanceFactory->create());
+        $instance = $cache->get('instance', function (ItemInterface $item) use ($instanceFactory) {
+            return $instanceFactory->create();
+        });
+
+        return new JsonResponse($instance);
     }
 }
