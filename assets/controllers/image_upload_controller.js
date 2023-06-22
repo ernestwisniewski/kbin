1b8ddc108bd3f17ecc0cb8a1cee4bb3f8e56eb7b
driedmoist <fr34kypwnz0r@gmail.com>
2023-06-22 16:41:46 -0700

    moved image preview js to controller

---
+++ assets/controllers/image_upload_controller.js
@@ -0,0 +1,36 @@
+import {Controller} from '@hotwired/stimulus';
+
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+    connect() {
+        const container = this.element;
+        const input = container.querySelector('.image-input');
+        const preview = container.querySelector('.image-preview');
+        const clearButton = container.querySelector('.image-preview-clear');
+        
+        input.addEventListener('change', function(e) {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            
+            reader.onload = function(e) {
+                preview.src = e.target.result;
+                preview.style.display = 'block';
+                clearButton.setAttribute('style', 'display: inline-block !important');
+            };
+            
+            reader.readAsDataURL(file);
+        });
+    }
+
+    clearPreview() {
+        const container = this.element;
+        const input = container.querySelector('.image-input');
+        const preview = container.querySelector('.image-preview');
+        const clearButton = container.querySelector('.image-preview-clear');
+
+        input.value = '';
+        preview.src = '#';
+        preview.style.display = 'none';
+        clearButton.style.display = 'none';
+    }
+}
