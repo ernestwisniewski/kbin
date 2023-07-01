677501d8461a3a8ab9bc0d88ecd57bf16b18465d
Szymon Kamiński <kaminskisj@gmail.com>
2023-07-01 21:01:04 +0200

    Include all unicode word chars in LOCAL_TAG regex

--- src/Utils/RegPatterns.php
+++ src/Utils/RegPatterns.php
@@ -12,5 +12,5 @@ class RegPatterns
     public const LOCAL_USER = '/^@[a-zA-Z0-9_-]{1,30}\b/';
     public const AP_MAGAZINE = '/^(!\w{2,25})(@)(([a-z0-9|-]+\.)*[a-z0-9|-]+\.[a-z]+)/';
     public const AP_USER = '/^(@\w{1,30})(@)(([a-z0-9|-]+\.)*[a-z0-9|-]+\.[a-z]+)/';
-    public const LOCAL_TAG = '/\B#([a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ0-9_]{2,45})/';
+    public const LOCAL_TAG = '/\B#(\w{2,45})/u';
 }
