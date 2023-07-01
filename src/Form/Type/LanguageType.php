f150f52fed39b866dd97e1b6c6dc2df047e7e2af
Adam Cooper <adam@acpr.dev>
2023-07-01 17:47:52 +0100

    Use Symfony list of languages. It's large. Maybe not?

--- src/Form/Type/LanguageType.php
+++ src/Form/Type/LanguageType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\Form\ChoiceList\Loader\CallbackChoiceLoader;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Intl\Exception\MissingResourceException;
+use Symfony\Component\Intl\Languages;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\UX\Autocomplete\Form\AsEntityAutocompleteField;
@@ -21,25 +23,6 @@ class LanguageType extends AbstractType
     ) {
     }
 
-    public static array $choices = [
-        'cz' => 'czech',
-        'dk' => 'danish',
-        'nl' => 'dutch',
-        'en' => 'english',
-        'fi' => 'finnish',
-        'fr' => 'french',
-        'de' => 'german',
-        'gr' => 'greek',
-        'he' => 'hebrew',
-        'hu' => 'hungarian',
-        'it' => 'italian',
-        'ja' => 'japanese',
-        'pl' => 'polish',
-        'pt' => 'portuguese',
-        'es' => 'spanish',
-        'uk' => 'ukrainian',
-    ];
-
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(
@@ -48,11 +31,18 @@ class LanguageType extends AbstractType
                     return ChoiceList::loader($this, new CallbackChoiceLoader(function () {
                         $preferredLanguages = $this->security->getUser()->preferredLanguages;
 
+                        foreach (Languages::getLanguageCodes() as $languageCode) {
+                            try {
+                                $choices[$languageCode] = Languages::getName($languageCode, $languageCode);
+                            } catch (MissingResourceException) {
+                            }
+                        }
+
                         $choices = 0 < count($preferredLanguages)
-                            ? array_filter(static::$choices, function ($code) use ($preferredLanguages) {
+                            ? array_filter($choices, function ($code) use ($preferredLanguages) {
                                 return in_array($code, $preferredLanguages);
                             }, ARRAY_FILTER_USE_KEY)
-                            : static::$choices;
+                            : $choices;
                         
                         return array_flip($choices);
                     }));
