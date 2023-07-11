5ea0a607bf29268257197dabff47312e40f8bc4a
Daniel Pervan <daniel@pervanovo.se>
2023-07-11 14:56:48 +0200

    Updated terminology to align with Symfony lingo

--- src/Twig/Components/SettingsRowEnumComponent.php
+++ src/Twig/Components/SettingsRowEnumComponent.php
@@ -7,8 +7,8 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent ('settings_row_enum', template: 'components/_settings_row_enum.html.twig')]
 class SettingsRowEnumComponent
 {
-    public string $title;
-    public string $description;
+    public string $label;
+    public string $help;
     public string $settingsKey;
     public array $values;
     public string $defaultValue;
--- src/Twig/Components/SettingsRowSwitchComponent.php
+++ src/Twig/Components/SettingsRowSwitchComponent.php
@@ -7,8 +7,8 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent ('settings_row_switch', template: 'components/_settings_row_switch.html.twig')]
 class SettingsRowSwitchComponent
 {
-    public string $title;
-    public string $description;
+    public string $label;
+    public string $help;
     public string $settingsKey;
     public bool $defaultValue = false;
 }
