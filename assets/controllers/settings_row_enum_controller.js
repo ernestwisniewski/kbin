f58f1bf8d124491e8bddee024819b15db09e3b70
Daniel Pervan <daniel@pervanovo.se>
2023-07-11 15:07:03 +0200

    Code cleanup and added comments

--- assets/controllers/settings_row_enum_controller.js
+++ assets/controllers/settings_row_enum_controller.js
@@ -1,9 +1,11 @@
 import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
-    change({params}) {
-        const {actionPath} = params;
-        console.log("change", params);
-        fetch(actionPath);
+    /**
+     * Calls the action at the given path when the value changes
+     * @param actionPath {string} - The path to the action to be called
+     */
+    change({params: {actionPath}}) {
+        return fetch(actionPath);
     }
 }
--- assets/controllers/settings_row_switch_controller.js
+++ assets/controllers/settings_row_switch_controller.js
@@ -1,8 +1,14 @@
 import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
+    /**
+     * Calls the action at the given path when the toggle is checked or unchecked
+     * @param target {HTMLInputElement} - The checkbox element of the toggle that was clicked
+     * @param truePath {string} - The path to the action to be called when the toggle is checked
+     * @param falsePath {string} - The path to the action to be called when the toggle is unchecked
+     */
     toggle({target, params: {truePath, falsePath}}) {
         const path = target.checked ? truePath : falsePath;
-        fetch(path);
+        return fetch(path);
     }
 }
