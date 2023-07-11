76310382810658700bde1bcc0ca7f5820f8547d9
Szymon Kamiński <kaminskisj@gmail.com>
2023-07-11 20:25:56 +0200

    Use MagazinePageView as form DTO

--- src/PageView/MagazinePageView.php
+++ src/PageView/MagazinePageView.php
@@ -8,10 +8,45 @@ use App\Repository\Criteria;
 
 class MagazinePageView extends Criteria
 {
-    public const SORT_OPTIONS = [
-        self::SORT_ACTIVE,
-        self::SORT_HOT,
-        self::SORT_NEW,
-        self::SORT_TOP,
-    ];
+    public const SORT_THREADS = 'threads';
+    public const SORT_COMMENTS = 'comments';
+    public const SORT_POSTS = 'posts';
+
+    public const FIELDS_NAMES = 'names';
+    public const FIELDS_NAMES_DESCRIPTIONS = 'names_descriptions';
+
+    public const ADULT_HIDE = 'hide';
+    public const ADULT_SHOW = 'show';
+    public const ADULT_ONLY = 'only';
+
+    public ?string $query = null;
+    public string $fields = self::FIELDS_NAMES;
+
+    public function __construct(
+        public int $page,
+        public string $sortOption,
+        public string $federation,
+        public string $adult,
+    )
+    {
+        parent::__construct($page);
+        $this->resolveSort($sortOption);
+    }
+
+    public function showOnlyLocalMagazines(): bool
+    {
+        return $this->federation === self::AP_LOCAL;
+    }
+
+    protected function routes(): array
+    {
+        return array_merge(
+            parent::routes(),
+            [
+                'threads' => self::SORT_THREADS,
+                'comments' => self::SORT_COMMENTS,
+                'posts' => self::SORT_POSTS,
+            ],
+        );
+    }
 }
