99da23da1a3c4e44d07acf52fec325323e494d79
Ernest Wiśniewski <ernestwisniewski2@gmail.com>
2023-07-15 05:46:51 +0200

    Doctrine remember me token provider

---
+++ migrations/Version20230715034515.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230715034515 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE rememberme_token (series VARCHAR(88) NOT NULL, value VARCHAR(88) NOT NULL, lastUsed TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, class VARCHAR(100) NOT NULL, username VARCHAR(200) NOT NULL, PRIMARY KEY(series))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE rememberme_token');
+    }
+}
