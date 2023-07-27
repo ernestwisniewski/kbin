4d81b090d1f62cdb77bbb838c6a05cf6e2e60149
Ernest Wiśniewski <ernestwisniewski2@gmail.com>
2023-07-27 09:03:07 +0200

    Limitation of random sidebar content

--- src/Repository/EntryRepository.php
+++ src/Repository/EntryRepository.php
@@ -373,6 +373,7 @@ class EntryRepository extends ServiceEntityRepository implements TagRepositoryIn
             ->where('e.isAdult = false')
             ->andWhere('e.visibility = :visibility')
             ->andWhere('m.isAdult = false')
+            ->andWhere('e.apId IS NULL')
             ->join('e.magazine', 'm')
             ->orderBy('e.createdAt', 'DESC')
             ->setParameters(['visibility' => VisibilityInterface::VISIBILITY_VISIBLE])
