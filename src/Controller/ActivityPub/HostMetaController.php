70f88059e4a724db1f9d2ee7208a1e93746fb68f
Adam Cooper <adam@acpr.dev>
2023-08-16 08:27:36 +0000

    Implement host-meta endpoint and increase node-info discoverability (#928)
    I noticed that I was having requests coming in for the node-info v2 endpoint with a .json suffix. So I amended the route to also accept that.
    
    Additionally the host-meta endpoint was returning a blank json document rather than the RFC specificed XRD xml doc. So I implemented that.
    
    Reviewed-on: https://codeberg.org/Kbin/kbin-core/pulls/928
    Co-authored-by: Adam Cooper <adam@acpr.dev>
    Co-committed-by: Adam Cooper <adam@acpr.dev>

--- src/Controller/ActivityPub/HostMetaController.php
+++ src/Controller/ActivityPub/HostMetaController.php
@@ -4,16 +4,52 @@ declare(strict_types=1);
 
 namespace App\Controller\ActivityPub;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
+/**
+ * Implementation of RFC 6415 host-meta file.
+ *
+ * @see https://datatracker.ietf.org/doc/html/rfc6415
+ */
 class HostMetaController
 {
-    public function __construct()
-    {
+    public function __construct(
+        private readonly UrlGeneratorInterface $urlGenerator,
+    ) {
     }
 
-    public function __invoke(): JsonResponse
+    public function __invoke(): Response
     {
-        return new JsonResponse();
+        $document = new \XMLWriter();
+        $document->openMemory();
+        $document->startDocument('1.0', 'UTF-8');
+
+        $document->startElement('XRD');
+        $document->writeAttribute('xmlns', 'http://docs.oasis-open.org/ns/xri/xrd-1.0');
+
+        $document->startElement('Link');
+        $document->writeAttribute('rel', 'lrdd');
+        $document->writeAttribute('type', 'application/jrd+json');
+        $document->writeAttribute(
+            'template',
+            $this->urlGenerator->generate(
+                'ap_webfinger',
+                [],
+                $this->urlGenerator::ABSOLUTE_URL,
+            ).'?resource={uri}'
+        );
+
+        $document->endElement(); // Link
+        $document->endElement(); // XRD
+        $document->endDocument();
+
+        return new Response(
+            $document->outputMemory(),
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'application/xrd+xml',
+            ],
+        );
     }
 }
