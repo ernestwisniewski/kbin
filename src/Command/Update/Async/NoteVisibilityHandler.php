b90ed676401c02ccb6b415207f63ce5c8ee89b23
Adam Cooper <adam@acpr.dev>
2023-09-12 06:37:56 +0000

    Use symfony configuration options to set a globle User-Agent for outbound requests. (#1098)
    Currently this is done in a single place by overriding the request headers value. It should be done everywhere so this ensures that *all* outbound curl requests are tagged with the kbin useragent string without additional per-implementation work.
    
    Reviewed-on: https://codeberg.org/Kbin/kbin-core/pulls/1098
    Co-authored-by: Adam Cooper <adam@acpr.dev>
    Co-committed-by: Adam Cooper <adam@acpr.dev>

--- src/Command/Update/Async/NoteVisibilityHandler.php
+++ src/Command/Update/Async/NoteVisibilityHandler.php
@@ -5,19 +5,17 @@ namespace App\Command\Update\Async;
 use App\Entity\Contracts\VisibilityInterface;
 use App\Entity\Post;
 use App\Entity\PostComment;
-use App\Service\SettingsManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsMessageHandler]
-class NoteVisibilityHandler
+readonly class NoteVisibilityHandler
 {
     public function __construct(
-        private readonly EntityManagerInterface $entityManager,
-        private readonly HttpClientInterface $client,
-        private readonly SettingsManager $settingsManager
+        private EntityManagerInterface $entityManager,
+        private HttpClientInterface $client,
     ) {
     }
 
@@ -32,7 +30,6 @@ class NoteVisibilityHandler
         $req = $this->client->request('GET', $entity->apId, [
             'headers' => [
                 'Accept' => 'application/activity+json,application/ld+json,application/json',
-                'User-Agent' => 'kbinBot/0.1 (+https://'.$this->settingsManager->get('KBIN_DOMAIN').'/bot)',
             ],
         ]);
 
