8419644b5c52f9f852c39e7220ce7398693f2e95
Szymon Kamiński <kaminskisj@gmail.com>
2023-10-17 17:17:06 +0000

    Hide magazines blocked by user in autocomplete (#1196)
    Alternative implementation of idea suggested by @AnonymousLlama in #853.
    Uses less queries and fixes dead code as mentioned in CR of that PR.
    
    Reviewed-on: https://codeberg.org/Kbin/kbin-core/pulls/1196
    Co-authored-by: Szymon Kamiński <kaminskisj@gmail.com>
    Co-committed-by: Szymon Kamiński <kaminskisj@gmail.com>

--- src/Form/Type/MagazineAutocompleteType.php
+++ src/Form/Type/MagazineAutocompleteType.php
@@ -6,7 +6,9 @@ namespace App\Form\Type;
 
 use App\Entity\Contracts\VisibilityInterface;
 use App\Entity\Magazine;
+use App\Entity\MagazineBlock;
 use Doctrine\ORM\QueryBuilder;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\UX\Autocomplete\Form\AsEntityAutocompleteField;
@@ -15,6 +17,10 @@ use Symfony\UX\Autocomplete\Form\ParentEntityAutocompleteType;
 #[AsEntityAutocompleteField]
 class MagazineAutocompleteType extends AbstractType
 {
+    public function __construct(private readonly Security $security)
+    {
+    }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
@@ -22,6 +28,17 @@ class MagazineAutocompleteType extends AbstractType
             'choice_label' => 'name',
             'placeholder' => 'select_magazine',
             'filter_query' => function (QueryBuilder $qb, string $query) {
+                if ($currentUser = $this->security->getUser()) {
+                    $qb
+                        ->andWhere(
+                            sprintf(
+                                'entity.id NOT IN (SELECT IDENTITY(mb.magazine) FROM %s mb WHERE mb.user = :user)',
+                                MagazineBlock::class,
+                            )
+                        )
+                        ->setParameter('user', $currentUser);
+                }
+
                 if (!$query) {
                     return;
                 }
@@ -29,9 +46,9 @@ class MagazineAutocompleteType extends AbstractType
                 $qb->andWhere('entity.name LIKE :filter OR entity.title LIKE :filter')
                     ->andWhere('entity.apId IS NULL')
                     ->andWhere('entity.visibility = :visibility')
-                    ->setParameters(
-                        ['filter' => '%'.$query.'%', 'visibility' => VisibilityInterface::VISIBILITY_VISIBLE]
-                    );
+                    ->setParameter('filter', '%'.$query.'%')
+                    ->setParameter('visibility', VisibilityInterface::VISIBILITY_VISIBLE)
+                ;
             },
         ]);
     }
