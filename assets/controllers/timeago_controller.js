9d241f85991b7b43498c3bb4fa1e35b66a966098
Ernest Wiśniewski <ernestwisniewski2@gmail.com>
2023-11-17 15:13:45 +0000

    Translations sync (#1270)
    Reviewed-on: https://codeberg.org/Kbin/kbin-core/pulls/1270
    Co-authored-by: Ernest Wiśniewski <ernestwisniewski2@gmail.com>
    Co-committed-by: Ernest Wiśniewski <ernestwisniewski2@gmail.com>

--- assets/controllers/timeago_controller.js
+++ assets/controllers/timeago_controller.js
@@ -1,16 +1,20 @@
 import {Controller} from '@hotwired/stimulus';
 import * as timeago from 'timeago.js';
+import da from 'timeago.js/lib/lang/da';
 import de from 'timeago.js/lib/lang/de';
 import el from 'timeago.js/lib/lang/el';
 import eo from '../utils/timeago-esperanto';
 import es from 'timeago.js/lib/lang/es';
 import fr from 'timeago.js/lib/lang/fr';
+import gl from 'timeago.js/lib/lang/gl';
 import it from 'timeago.js/lib/lang/it';
 import ja from 'timeago.js/lib/lang/ja';
 import nl from 'timeago.js/lib/lang/nl';
 import pl from 'timeago.js/lib/lang/pl';
 import pt from 'timeago.js/lib/lang/pt_BR';
+import ru from 'timeago.js/lib/lang/ru';
 import tr from 'timeago.js/lib/lang/tr';
+import uk from 'timeago.js/lib/lang/uk';
 import zh_TW from 'timeago.js/lib/lang/zh_TW';
 
 /* stimulusFetch: 'lazy' */
@@ -23,7 +27,7 @@ export default class extends Controller {
         }
 
         const lang = document.documentElement.lang;
-        const languages = { de, el, eo, es, fr, it, ja, nl, pl, pt, tr, zh_TW };
+        const languages = { da, de, el, eo, es, fr, gl, it, ja, nl, pl, pt, ru, tr, zh_TW, uk };
 
         if (languages[lang]) {
             timeago.register(lang, languages[lang]);
